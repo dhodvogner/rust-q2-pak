acf7c2caa461bd4ec3dca33956ec9fbe51d33abf
Daniel Hodvogner <dhodvogner@gmail.com>
2019-07-25 20:22:22 +0200

    feat: export files

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ extern crate binary_rw;
 
 use binary_rw::{BinaryReader, OpenType};
 use std::env;
+use std::error::Error;
+use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 use std::process;
 
 fn main() {
-  println!("Rust Quake/Quake2 PAK explorer");
+  println!("Rust Quake/Quake2 PAK parser");
 
   let args: Vec<String> = env::args().collect();
   if args.len() == 1 {
@@ -49,11 +54,46 @@ fn main() {
       "#{} File Name: '{}' (Offset: {} Size: {})",
       x, file_name, offset, size
     );
+    export_file(&mut binary_file, file_name, offset, size);
   }
 }
 
-// Print the current postion
-// fn dump_pos(binary_file: &mut binary_rw::BinaryReader) {
-//   let pos = binary_file.get_cur_pos();
-//   println!("pos: {}", pos);
-// }
+fn export_file(
+  binary_file: &mut binary_rw::BinaryReader,
+  file_name: String,
+  offset: i32,
+  size: i32,
+) {
+  let out_file_name = String::from(&format!("out/{}", file_name));
+  let out_file_name = out_file_name.trim_matches(char::from(0));
+
+  let prev_pos = binary_file.get_cur_pos();
+  // Read file content
+  binary_file.seek_to(offset as u64);
+  let data = binary_file.read_bytes(size as u64);
+  binary_file.seek_to(prev_pos);
+
+  let path = Path::new(&out_file_name);
+  let display = path.display();
+  let parent = path.parent().unwrap();
+  let parent_display = parent.display();
+
+  match fs::create_dir_all(&parent) {
+    Err(why) => panic!(
+      "Couldn't create folders {}: {}",
+      parent_display,
+      why.description()
+    ),
+    Ok(_) => println!("Successfully created folders {}", parent_display),
+  };;
+
+  let mut file = match File::create(&path) {
+    Err(why) => panic!("Couldn't create {}: {}", display, why.description()),
+    Ok(file) => file,
+  };
+
+  match file.write_all(&data) {
+    Err(why) => panic!("Couldn't write to {}: {}", display, why.description()),
+    Ok(_) => println!("Successfully wrote to {}", display),
+  }
+}
